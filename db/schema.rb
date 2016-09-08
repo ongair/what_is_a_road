93ea72129c92154ef1ad377bbcf42cd0f6661a0f
Muaad <muadh24@gmail.com>
2016-09-08 15:08:44 +0300

    Generated model for Road

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908115954) do
+ActiveRecord::Schema.define(version: 20160908120821) do
+
+  create_table "roads", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
