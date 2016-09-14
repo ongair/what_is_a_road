837b83c99420c0908ba1b168b20f1e6f9ee0a29c
Muaad <muadh24@gmail.com>
2016-09-14 15:05:14 +0300

    Added address to Report

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909154019) do
+ActiveRecord::Schema.define(version: 20160914120447) do
 
   create_table "photos", force: :cascade do |t|
     t.string   "file_url"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160909154019) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.boolean  "complete",   default: false
+    t.string   "address"
   end
 
   add_index "reports", ["road_id"], name: "index_reports_on_road_id"
